947507323c6d56c577b95d49e668f57bb09b50a3
SHEFFmachine <sheff@sheffmachine.com>
2017-09-03 21:57:06 -0700

    selectionSort

--- GeeksForGeeks/Program.cs
+++ GeeksForGeeks/Program.cs
@@ -8,6 +8,9 @@ namespace GeeksForGeeks
         {
             var swap = new Swap();
             swap.Run(5, 10);
+
+            var selectionSort = new SelectionSort();
+            selectionSort.Run(new int[] {64, 25, 12, 22, 11});
         }
     }
 }
---
+++ GeeksForGeeks/SelectionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace GeeksForGeeks
+{
+    public class SelectionSort
+    {
+		//The sort algorithm has a sorted and unsorted sets.
+		//The algorithm moves through the unsorted sections and moves the minimum element to the front
+
+		//Input: [64, 25, 12, 22, 11]
+
+		//Pass 1: [11, 25, 12, 22, 64]
+		//Pass 2: [11, 12, 25, 22, 64]
+		//Pass 3: [11, 12, 22, 25, 64]
+        //Pass 4: [11, 12, 22, 25, 64] => You can see this is redundant, could be optimized
+
+
+
+
+		public void Run(int[] inputArr)
+        {
+            var length = inputArr.Length;
+
+			//The unexplored region starts at the length of the array
+			for (int i = 0; i < length; i++)
+            {
+                //We move explored region forward by 1 every iteration by moving along the i
+                var lowestItemIndex = i;
+
+				//Explored region moves ahead by 1 item along with the i.
+				for (int j = i + 1; j < length; j++)
+                {
+					if (inputArr[j] < inputArr[lowestItemIndex])
+                    {
+                        var temp = inputArr[j];
+                        inputArr[j] = inputArr[lowestItemIndex];
+                        inputArr[lowestItemIndex] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in inputArr)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
