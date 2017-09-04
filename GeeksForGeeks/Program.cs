5d5f2f91e93a48bf364075c9137046e92d73e005
SHEFFmachine <sheff@sheffmachine.com>
2017-09-03 21:18:21 -0700

    initial commit

---
+++ GeeksForGeeks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeeksForGeeks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
