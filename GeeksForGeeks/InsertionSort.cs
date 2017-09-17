715acf472fea1c0a94c51b2304bc7cd4b591bfd4
SHEFFmachine <sheff@sheffmachine.com>
2017-09-16 17:26:46 -0700

    update insertion sort

--- GeeksForGeeks/InsertionSort.cs
+++ GeeksForGeeks/InsertionSort.cs
@@ -3,49 +3,60 @@ namespace GeeksForGeeks
 {
     public class InsertionSort
     {
-        //Insertion sort works in a similar way to how we sort playing cards in our hands.
+		//Insertion sort works in a similar way to how we sort playing cards in our hands.
+		//We divide our input into a sorted portion and an unsorted portion.
 
-        //Time Complexity: O(n^2).
-        //Auxilary Space:  O(1).
+		//Time Complexity: O(n^2).
+		//Auxilary Space:  O(1).
 
-        //Max time when items are sorted in reverse order.  Minimum time when the items are fully sorted.
-        //Often used when the number of items is small, and when they are already almost fully sorted.
-
-
-        //Example:
-
-        //Input:  [7, 8, 5, 2, 4, 6, 3]
-
-        //Pass 1: [7, 8, 5, 2, 4, 6, 3]
-        //Pass 2: [5, 7, 8, 2, 4, 6, 3]
-        //Pass 3: [2, 5, 7, 8, 4, 6, 3]
-        //Pass 4: [2, 4, 5, 7, 8, 6, 3]
-        //Pass 5: [2, 4, 5, 6, 7, 8, 3]
-        //Pass 6: [2, 3, 4, 5, 6, 7, 8]
+		//Max time when items are sorted in reverse order.  Minimum time when the items are fully sorted O(n).
+		//Often used when the number of items is small, and when they are already almost fully sorted.
+		//It is a little more efficient then selection sort or bubble sort in "practical" scenarios.
 
 
-        //Links:
-        //https://www.youtube.com/watch?v=DFG-XuyPYUQ -CS50
+		//Example:
+
+		//Input: [23, 42, 4, 16, 8, 15] => Since we haven't started sorting yet, the whole list is unsorted.
+
+		//        |srtd| |---- unsrtd----|
+		//Pass 1: [23, | 42, 4, 16, 8, 15] => Let's consider the first element to be the begining of our sorted region.
+		//        |-- srtd-| |-- unsrtd--|
+		//Pass 2: [23, 42, | 4, 16, 8, 15] => We look at the first number in the sorted section, and try to place it into the sorted region.
+		//        |-- srtd ---| |-usrtd--|    We do this by comparing the first number in unsorted seciton and the inserting it into the right place in sorted region.
+		//Pass 3: [4, 23, 42,|  16, 8, 15] => Here we take 4, and see where to place it, then move srted region members to the right if they are larger then 4. Then drop in 4.
+		//        |--- srtd ------| |unsr|
+		//Pass 4: [4, 16, 23, 42, | 8, 15] => here we take 16, move the soted elements greater then it to the irght and then dorp it into the approriate area.
+		//        |---- srted -------||us|
+		//Pass 5: [4, 8, 16, 23, 42, | 15] => 8
+		//        |-------- sorted ----|
+		//Pass 6: [4, 8, 15,  16, 23, 42] => array is fully sorted so our algorithm ends.
 
 
-        public void Run(int[] inputArr)
+		//Links:
+		//https://www.youtube.com/watch?v=DFG-XuyPYUQ - CS50
+		//https://www.youtube.com/watch?v=i-SKeOcBwko - my code school video
+		//https://www.youtube.com/watch?v=OGzPmgsI-pQ - G4G Video
+		//http://www.geeksforgeeks.org/insertion-sort/ - G4G written explanation
+
+
+		public void Run(int[] inputArr)
         {
-            var length = inputArr.Length;
+            var length = inputArr.Length; // Just to make our lives simpler.
 
             //we want to start at 1 because the first element cannot be greater or less then itself.
             for (int i = 1; i < length; i++)
             {
                 var key = inputArr[i]; // serves both as temp storage and as a key to compare current item with
-                var j = i - 1;
+                var j = i - 1; // index of the item to the left of the current item for comparison. This is the first item in the sorted section
 
-                //We want to make j >= to 0 because we don't want to stop sorting at the first element, want to go to the end.
+                //We want to make j >= to 0 because we don't want to stop sorting at the first element, want to be able to go to the front and insert there.
                 while (j >= 0 && key < inputArr[j])
                 {
-                    //The reason why we go ++ is that we want to move the value to the right there before examining the next item with --
+                    //This is the wapping section, this will grab an element to the right (which is the current element in the prev loop and move it right.
                     inputArr[j + 1] = inputArr[j];
-                    j--;
+                    j--; //then we go left one.
                 }
-                //The reason why we do ++ here is because we want to return to previous poisition from going back one i j-- above.
+                //when we exit the while loop it means that we found where to drop the element and we are one to the left of where that place is.
                 inputArr[j + 1] = key;
             }
 
--- GeeksForGeeks/Program.cs
+++ GeeksForGeeks/Program.cs
@@ -6,23 +6,23 @@ namespace GeeksForGeeks
     {
         public static void Main(string[] args)
         {
-            var swap = new Swap();
-            swap.Run(5, 10);
+            //var swap = new Swap();
+            //swap.Run(5, 10);
 
-            var selectionSort = new SelectionSort();
-            selectionSort.Run(new int[] {64, 25, 12, 22, 11});
+            //var selectionSort = new SelectionSort();
+            //selectionSort.Run(new int[] {64, 25, 12, 22, 11});
 
             var insertionSort = new InsertionSort();
-            insertionSort.Run(new int[]{7, 8, 5, 2, 4, 6, 3 });
+            insertionSort.Run(new int[]{ 23, 42, 4, 16, 8, 15 });
 
-            var binarySearch = new BinarySearch();
-            binarySearch.RunRecursive(new int[] { 0, 2, 3, 4, 10, 40, 44 }, 2, 0, 6);
-            binarySearch.RunIterative(new int[] { 0, 2, 3, 4, 10, 40, 44 }, 2);
+            //var binarySearch = new BinarySearch();
+            //binarySearch.RunRecursive(new int[] { 0, 2, 3, 4, 10, 40, 44 }, 2, 0, 6);
+            //binarySearch.RunIterative(new int[] { 0, 2, 3, 4, 10, 40, 44 }, 2);
 
 
 
-            var BFSGraph = new BFSGraph();
-            BFSGraph.BFS(2);
+            //var BFSGraph = new BFSGraph();
+            //BFSGraph.BFS(2);
         }
     }
 }
