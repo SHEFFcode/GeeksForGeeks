50eea8f33a571ad54b2428dccb85f377ed77e9aa
SHEFFmachine <sheff@sheffmachine.com>
2017-10-07 15:24:40 -0700

    start on ditkas algorithm

---
+++ GeeksForGeeks/Greedy/DitkasAlgo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace GeeksForGeeks.Greedy
+{
+    public class DitkasAlgo
+    {
+		// Single source shortest path
+		// Works in directed or undirected, but only with non negative weights
+		// Runtime complexity is O(Elog(v)), with E being edges, and v is vertecies
+
+
+		// Links:
+		// https://www.youtube.com/watch?v=GazC3A4OQTE - computerphile
+		// https://www.youtube.com/watch?v=U9Raj6rAqqs - Joe James
+		// https://www.youtube.com/watch?v=qx9sJ3O3JM0 - bootstrappers
+	 }
+}
