3d7ae3adacc1590f1a1f067ffd5c0c65b400e03a
SHEFFmachine <sheff@sheffmachine.com>
2017-10-07 17:58:06 -0700

    implement ditkas algorithm

--- GeeksForGeeks/Program.cs
+++ GeeksForGeeks/Program.cs
@@ -38,9 +38,20 @@ namespace GeeksForGeeks
             //var BFSGraph = new BFSGraph();
             //BFSGraph.BFS(2);
 
-            var activitySelection = new ActivitySelection();
-            activitySelection.Run(new int[] {10, 12, 20}, new int[] {20, 25, 30});
+            //var activitySelection = new ActivitySelection();
+            //activitySelection.Run(new int[] {10, 12, 20}, new int[] {20, 25, 30});
 
+            var ditka = new DitkasAlgo();
+            ditka.Run(new int[][]{new int[] {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                                  new int[]{4, 0, 8, 0, 0, 0, 0, 11, 0},
+                                  new int[]{0, 8, 0, 7, 0, 4, 0, 0, 2},
+                                  new int[]{0, 0, 7, 0, 9, 14, 0, 0, 0},
+                                  new int[]{0, 0, 0, 9, 0, 10, 0, 0, 0},
+                                  new int[]{0, 0, 4, 14, 10, 0, 2, 0, 0},
+                                  new int[]{0, 0, 0, 0, 0, 2, 0, 1, 6},
+                                  new int[]{8, 11, 0, 0, 0, 0, 1, 0, 7},
+                                  new int[]{0, 0, 2, 0, 0, 0, 6, 7, 0}
+            }, 0);
         }
     }
 }
