6541a5804c5fa30068336fb68196731065dd9555
SHEFFmachine <sheff@sheffmachine.com>
2017-11-14 18:22:22 -0800

    reindent lines

--- GeeksForGeeks/Sorting/SelectionSort.cs
+++ GeeksForGeeks/Sorting/SelectionSort.cs
@@ -35,40 +35,40 @@ namespace GeeksForGeeks
     //https://www.youtube.com/watch?v=xWBP4lzkoyM - selection sort G4G
     //http://www.geeksforgeeks.org/selection-sort/ - G4G Explanation
 
-    public void Run(int[] inputArr)
-    {
-      var length = inputArr.Length;
-
-      //The unsorted region starts at the length of the array, moves on by 1
-      for (int i = 0; i < length; i++) //This can run till length -1 as another way to optimize
-      {
-        //Let's keep track of the lowest item's index by first setting it to i as a initial value.
-        var lowestItemIndex = i;
-
-        //The loop that finds the lowest element in the unsorted region of the array.
-        for (int j = i + 1; j < length; j++)
+        public void Run(int[] inputArr)
         {
-          if (inputArr[j] < inputArr[lowestItemIndex])
-          {
-            lowestItemIndex = j;
-          }
+            var length = inputArr.Length;
+
+            //The unsorted region starts at the length of the array, moves on by 1
+            for (int i = 0; i < length; i++) //This can run till length -1 as another way to optimize
+            {
+                //Let's keep track of the lowest item's index by first setting it to i as a initial value.
+                var lowestItemIndex = i;
+
+                //The loop that finds the lowest element in the unsorted region of the array.
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (inputArr[j] < inputArr[lowestItemIndex])
+                    {
+                        lowestItemIndex = j;
+                    }
+                }
+
+                //This is an optimization to prevent swapping if lowest index is equal to i.
+                //If i is equal to lowest item idex, no swap is necessary.
+                if (lowestItemIndex != i)
+                {
+                    var temp = inputArr[lowestItemIndex];
+                    inputArr[lowestItemIndex] = inputArr[i];
+                    inputArr[i] = temp;
+                }
+
+            }
+
+            foreach (var item in inputArr)
+            {
+                Console.WriteLine($"{item}");
+            }
         }
-
-        //This is an optimization to prevent swapping if lowest index is equal to i.
-        //If i is equal to lowest item idex, no swap is necessary.
-        if (lowestItemIndex != i)
-        {
-          var temp = inputArr[lowestItemIndex];
-          inputArr[lowestItemIndex] = inputArr[i];
-          inputArr[i] = temp;
-        }
-
-      }
-
-      foreach (var item in inputArr)
-      {
-        Console.WriteLine($"{item}");
-      }
     }
-  }
 }
