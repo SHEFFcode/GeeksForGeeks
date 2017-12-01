3b62a239355aba8d855a2f09f851dff74daf6122
SHEFFmachine <sheff@sheffmachine.com>
2017-11-30 20:41:08 -0800

    Min number of coins code solution

--- GeeksForGeeks/Greedy/MinNumberOfCoins.cs
+++ GeeksForGeeks/Greedy/MinNumberOfCoins.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GeeksForGeeks.Greedy
 {
     public class MinNumberOfCoins
     {
-        public MinNumberOfCoins()
+        public void Run(int[] currency, int changeToGive)
         {
+            List<int> coinsUsed = new List<int>();  // This will contain the result
+
+            for (int i = currency.Length - 1; i > -1; i--)
+            {
+                while (changeToGive >= currency[i])  // while we still have some change to give out
+                {
+                    changeToGive -= currency[i]; // reduce the amount of change we still need to give
+                    coinsUsed.Add(currency[i]);  // add the coin use to the solution
+                }
+            }
+
+            foreach (var coin in coinsUsed)
+            {
+                Console.WriteLine(coin);
+            }
         }
     }
 }
